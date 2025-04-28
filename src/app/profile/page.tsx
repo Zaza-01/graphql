ce5f0eed4c366805364760a3f2f4f0b64c126094
mibrahim <moatazusama9@gmail.com>
2025-04-28 17:54:25 +0300

    :

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,7 +3,6 @@
 import { useQuery, gql } from '@apollo/client';
 import client from '../../lib/apolloClient';
 import { AuditRatioBarChart } from '@/components/AuditRatioChart';
-import React, { useState, useEffect } from 'react';
 import BarChart from '@/components/BarChart';
 import { XPLineChart } from '@/components/XPLineChart';
 import { Error } from '@/components/Error';
@@ -56,13 +55,7 @@ const USER_QUERY = gql`
 `;
 
 export default function Profile() {
-  // const [showSkeleton, setShowSkeleton] = useState(true);
   const { loading, error, data } = useQuery(USER_QUERY, { client });
-
-  // useEffect(() => {
-  //   const timer = setTimeout(() => setShowSkeleton(false), 1000); // Minimum skeleton time: 1s
-  //   return () => clearTimeout(timer);
-  // }, []);
 
   if (loading) return <Loading />;
   if (error) return <Error message={error.message} />;
@@ -80,16 +73,16 @@ export default function Profile() {
 
   return (
     <div className='profile-grid-container'>
-      {/* <button className='bg-black w-[6em] h-[3em] absolute top-0 right-0 mr-[1em] mt-[1em] flex justify-center items-center rounded-md hover:bg-slate-500 transition-all ease-in duration-0.3ms cursor-pointer' onClick={logout}>
-          <span className='font-title font-medium text-white'>Logout</span>
-        </button> */}
       <div className='profile-grid'>
         <div className='user-info card'>
           <h1 className='font-title title'>Welcome, {user[0].firstName} {user[0].lastName} 🏋🏽</h1>
           <div className="info">
             <p className='font-title'>&emsp;Your Email : <span className='font-text2 font-medium'>{user[0].email}</span></p>
             <p className='font-title'>&emsp;Your Username : <span className='font-text2 font-medium'>{user[0].login}</span></p>
-            <p className='font-title'>&emsp;Your Level : <span className='font-text2 font-medium'>{userLevel[0].level}</span></p>
+            <div className='flex justify-between'>
+              <p className='font-title'>&emsp;Your Level : <span className='font-text2 font-medium'>{userLevel[0].level}</span></p>
+              <svg onClick={logout} className='logoutIcon' viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#46b4ae"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.048"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.5" d="M15.9998 2L14.9998 2C12.1714 2 10.7576 2.00023 9.87891 2.87891C9.00023 3.75759 9.00023 5.1718 9.00023 8.00023L9.00023 16.0002C9.00023 18.8287 9.00023 20.2429 9.87891 21.1215C10.7574 22 12.1706 22 14.9976 22L14.9998 22L15.9998 22C18.8282 22 20.2424 22 21.1211 21.1213C21.9998 20.2426 21.9998 18.8284 21.9998 16L21.9998 8L21.9998 7.99998C21.9998 5.17157 21.9998 3.75736 21.1211 2.87868C20.2424 2 18.8282 2 15.9998 2Z" fill="#46b4ae"></path> <path fillRule="evenodd" clipRule="evenodd" d="M15.75 12C15.75 11.5858 15.4142 11.25 15 11.25L4.02744 11.25L5.98809 9.56943C6.30259 9.29986 6.33901 8.82639 6.06944 8.51189C5.79988 8.1974 5.3264 8.16098 5.01191 8.43054L1.51191 11.4305C1.34567 11.573 1.25 11.781 1.25 12C1.25 12.2189 1.34567 12.4269 1.51191 12.5694L5.01191 15.5694C5.3264 15.839 5.79988 15.8026 6.06944 15.4881C6.33901 15.1736 6.30259 14.7001 5.98809 14.4305L4.02744 12.75L15 12.75C15.4142 12.75 15.75 12.4142 15.75 12Z" fill="#46b4ae"></path> </g></svg>
+            </div>
           </div>
         </div>
         <div className='audit-ratio card'>
