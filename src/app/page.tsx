86da6dbbdf0a0b951a97fda85ae57acee86f95fa
mibrahim <moatazusama9@gmail.com>
2025-06-24 01:07:01 +0300

    added animation to errors during login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Login() {
   const [credentials, setCredentials] = useState({ username: '', password: '' });
   const [error, setError] = useState('');
   const router = useRouter();
+
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => setError(''), 4000);
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
+
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -53,9 +62,23 @@ export default function Login() {
             <input type="password" className="form__field" autoComplete='off' placeholder="Password" name="password" id='password' onChange={(e) => setCredentials({ ...credentials, password: e.target.value })} required />
             <label htmlFor="password" className="form__label">Password</label>
           </div>
+          <AnimatePresence>
+            {error && (
+              <motion.p
+                key="loginError"
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.3 }}
+                className="text-red-500 font-title text-[1.4em] mt-5"
+              >
+                {error}
+              </motion.p>
+            )}
+          </AnimatePresence>
           <button className="btn mt-5"><span>Login</span></button>
         </form>
-        {error && <p className='text-red-500 font-title text-[1.4em] mt-5'>{error}</p>}
+        {/* {error && <p className='text-red-500 font-title text-[1.4em] mt-5'>{error}</p>} */}
       </div>
     </div>
   );
